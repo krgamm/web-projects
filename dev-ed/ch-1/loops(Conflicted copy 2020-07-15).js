9c82851585e618dedcc300d4b234d6a8c2df888d
Kevin Gammenthaler <kevin.gammenthaler@gmail.com>
2020-07-16 12:36:35 -0500

    Added new practice project

---
+++ dev-ed/ch-1/loops(Conflicted copy 2020-07-15).js
@@ -0,0 +1,35 @@
+//For Loops
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i === 20) {
+//     console.log("Hey you hit 20!");
+//     continue;
+//   }
+//   //RUN SOME CODE
+//   console.log(i);
+// }
+
+// const user = {
+//   name: "Kevin",
+//   age: 23,
+//   subscribers: 200,
+//   money: "lolno",
+// };
+
+// for (let x in user) {
+//   console.log(user[x]);
+// }
+
+//WHILE, DO WHILE
+
+let i = 0;
+
+// while (i < 30) {
+//   console.log(i);
+//   i++;
+// }
+
+do {
+  console.log(i);
+  i++;
+} while (i > 10);
